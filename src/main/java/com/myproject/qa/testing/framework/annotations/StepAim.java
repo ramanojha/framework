a537f2147115c836eb0eee6f67c2274c9540aa15
Raman Ojha <ojha.raman99@gmail.com>
2020-04-28 14:57:29 +0530

    Modified the @Aim & Added @StepAim annotation

--- src/main/java/com/myproject/qa/testing/framework/annotations/StepAim.java
+++ src/main/java/com/myproject/qa/testing/framework/annotations/StepAim.java
@@ -4,10 +4,13 @@ import static java.lang.annotation.ElementType.METHOD;
 
 import java.lang.annotation.Target;
 
-
 @Target({METHOD})
-public @interface Aim {
-	
+public @interface StepAim {
 	/*Annotate the method with description*/
-	public String[] value();
+	public String[] value() default "";;
+	
+	/*Annotate the method with step and expected*/
+	public String step() default "";
+	public String expected() default "";
+		
 }
